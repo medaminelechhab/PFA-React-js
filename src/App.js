d0b9e77ccf899b9cf4fb871615e8fbd4adc9cad2
Mouaad Elkhadari <ELKHADARI_MOUAAD@emsi-edu.ma>
2024-05-09 15:01:44 +0100

    qfter pdqte login file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import NavBar from './components/navBar';
+import Login from './components/login';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
+
